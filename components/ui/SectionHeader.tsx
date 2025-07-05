0fcce3ed7b08fc2cbf743913a10020d875d0b189
Phương Nam <namdpse180259@fpt.edu.vn>
2025-07-05 18:09:21 +0700

    Updated SectionHeader.tsx

--- components/ui/SectionHeader.tsx
+++ components/ui/SectionHeader.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { Button } from '@/components/ui/button'
-import { ChevronRight, LucideIcon } from 'lucide-react'
+import { ChevronRight, DivideIcon as LucideIcon } from 'lucide-react'
 
 interface SectionHeaderProps {
     title: string
